e97626c304e378123219f9dd5e002b503db23e37
aleshins <27894180+aleshins@users.noreply.github.com>
2020-04-27 07:32:34 +0200

    Update removeOffscale.m

--- Functions/removeOffscale.m
+++ Functions/removeOffscale.m
@@ -3,8 +3,8 @@ function gx = removeOffscale(gx,displaySize,timeKernel)
     gx(or(gx<displaySize(1),gx>displaySize(2)))=nan; % remove off-scale events
     [gapStart, gapStop] = idx2intervals(isnan(gx),1); % find gaps
     % extend gaps
-    gapStart = gapStart + timeKernel;
-    gapStop = gapStop - timeKernel;
+    gapStart = gapStart - timeKernel;
+    gapStop = gapStop + timeKernel;
     % check that we are in range from 1 to track duration
     gapStart(gapStart<1) = 1;
     gapStop(gapStop>numel(gx)) = numel(gx);
